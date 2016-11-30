52d5251f7bf6a2835a20f27eef26ae712007f0fd
J Yegerlehner <jyegerlehner@yahoo.com>
2016-11-29 20:43:19 -0600

    Generation.

--- generategc.sh
+++ generategc.sh
@@ -1,2 +1,2 @@
-python generate.py --samples 80000 --wav_out_path generated_s_p311_140K.wav --fast_generation false --gc_channels=48 --gc_id=311 --gc_cardinality=377 /media/mass1/logs/gc/train/2016-11-19T15-25-07/model.ckpt-140994
+python generate.py --samples 80000 --wav_out_path generated_s_p311_197K.wav --fast_generation false --gc_channels=48 --gc_id=311 --gc_cardinality=377 /media/mass1/logs/gc/train/2016-11-19T15-25-07/model.ckpt-197000
 
