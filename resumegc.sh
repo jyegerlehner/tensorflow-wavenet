5e2034970f58915d7e13aea488ab65cdd096cdd6
J Yegerlehner <jyegerlehner@yahoo.com>
2017-02-26 22:51:34 -0600

    Training scripts.

--- resumegc.sh
+++ resumegc.sh
@@ -1,1 +1,1 @@
-python train.py --data_dir=/media/mass1/audio_data/VCTK-Corpus-stripped --logdir=/media/mass1/logs/tts_0/train/2017-01-03T19-46-39 --learning_rate=0.0004 --momentum=0.9 --optimizer=adam --gc_channels=48 --checkpoint_every=1000 --silence_threshold=0 --num_steps=500000
+python train.py --data_dir=/media/jd/ssd120/audio_data/VCTK-Corpus-stripped --logdir=/media/jd/ssd120/logs/tts_0/train/2017-02-24T18-24-15 --learning_rate=0.0002 --momentum=0.9 --optimizer=adam --gc_channels=48 --checkpoint_every=1000 --silence_threshold=0 --num_steps=1000000
