7199229afb73f431570886f259de6f41dcede1ea
jy <jyegerlehner@yahoo.com>
2017-03-11 20:06:40 -0600

    Scripts for model iteration.

--- traingc.sh
+++ traingc.sh
@@ -1,1 +1,1 @@
-python train.py --data_dir=/media/jd/ssd120/audio_data/VCTK-Corpus-stripped --logdir_root=/media/jd/ssd120/logs/tts_0 --learning_rate=0.001 --optimizer=adam --momentum=0.9 --gc_channels=48 --checkpoint_every=1000
+python train.py --data_dir=/media/mass1/audio_data/VCTK-Corpus-stripped --logdir_root=/media/mass1/logs/tts_0 --learning_rate=0.001 --optimizer=adam --momentum=0.9 --gc_channels=48 --checkpoint_every=1000
